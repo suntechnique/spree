48d24ae591148dbf56d8d81cfbdb2fe3cd634ea8
Vitaly <faetonvs@gmail.com>
2015-12-09 15:45:08 +0500

    d

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -7,7 +7,7 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'sass-rails', '~> 5.0.0'
 
 
-gem 'sqlite3'#, platforms: [:ruby, :mingw, :mswin, :x64_mingw]
+gem 'sqlite3', platforms: [:ruby, :mingw, :mswin, :x64_mingw]
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw]
 
 platforms :jruby do
